9616c09884e95a1d0b262b0ccdf82392be094560
Aditya Sharma <adityasharma2005123@gmail.com>
2025-03-18 23:35:33 +0530

    Final Changes for deployment

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const cors = require('cors');
-const app = express();
 const path = require('path');
 const dotenv = require('dotenv');
 
@@ -8,6 +7,11 @@ dotenv.config();
 
 // const port = process.env.PORT;
 const port = process.env.PORT || 8080;
+
+const app = express();
+
+
+const _dirname = path.resolve();
 
 const contestRoutes = require("./routes/contest");
 
@@ -17,10 +21,16 @@ app.use(express.json());
 
 app.use("/api", contestRoutes);
 
-app.get("/", (req, res) => {
-    res.json({
-        message: "Welcome to TLE CP Tracker!"
-    });
+// app.get("/", (req, res) => {
+//     res.json({
+//         message: "Welcome to TLE CP Tracker!"
+//     });
+// });
+
+app.use(express.static(path.join(_dirname, "/client/dist")));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(_dirname, "/client", "dist", "index.html"));
 });
 
 
